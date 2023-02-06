db0e21351d5527d43ee3e1a1aea7092d83ba8f55
mvc <>
2023-02-06 21:21:24 +0500

    Add Package

---
+++ Tests/YaBalabobaTests/YaBalabobaTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import YaBalaboba
+
+final class YaBalabobaTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(YaBalaboba().text, "Hello, World!")
+    }
+}
